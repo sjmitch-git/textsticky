626babb268d19178f622d15ef9ac20c0a3622e9b
sjmitch-git <sjmitch20@outlook.com>
2025-01-13 21:39:04 +0000

    logo

--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -14,7 +14,7 @@ export default function Nav() {
           <Link
             href="/"
             title={Labels.nav.create}
-            tabIndex={`${pathname === "/" ? -1 : 0}`}
+            tabIndex={pathname === "/" ? -1 : 0}
             className={`rounded-md text-light p-2 flex gap-2 items-center ${pathname === "/" ? "bg-primary cursor-none" : "bg-dark  hover:underline"} focus-visible:outline-accent`}
           >
             <FaEdit /> <span className="hidden md:inline-block">{Labels.nav.create}</span>
@@ -22,7 +22,7 @@ export default function Nav() {
           <Link
             href="/saved"
             title={Labels.nav.saved}
-            tabIndex={`${pathname === "/saved" ? -1 : 0}`}
+            tabIndex={pathname === "/saved" ? -1 : 0}
             className={`rounded-md text-light p-2 flex gap-2 items-center ${pathname === "/saved" ? "bg-primary cursor-none" : "bg-dark hover:underline"} focus-visible:outline-accent`}
           >
             <FaBookmark /> <span className="hidden md:inline-block">{Labels.nav.saved}</span>
